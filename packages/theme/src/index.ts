95b96a085759d38268821b6f5e85a267a28b6014
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-02-09 20:48:37 +0100

    ✨ Export ThemeProviderLoader

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -2,4 +2,5 @@ export * from './constants';
 export * from './helpers/isSpacingKey';
 export * from './helpers/makeStyles';
 export * from './ThemeProvider';
+export * from './ThemeProviderLoader';
 export * from './type';
