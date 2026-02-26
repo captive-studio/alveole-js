e4f8978eb82cee5fcf9a853d5a9d83221d3769fb
Corentin Couq <corentin.couq@captive.fr>
2026-02-26 10:34:12 +0100

    🚚 Dissocie les stories des components

---
+++ packages/components/src/stories/index.ts
@@ -0,0 +1,5 @@
+export * from '../core/Box/Box.stories';
+export { default as BoxStoryMeta } from '../core/Box/Box.stories';
+
+export * from '../core/Typography/Typography.stories';
+export { default as TypographyStoryMeta } from '../core/Typography/Typography.stories';
