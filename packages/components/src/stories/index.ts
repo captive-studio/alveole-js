3574abf6605e66937d42cc5f22599e71dc74ba3f
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-03-02 11:14:21 +0100

    🔧 Exporte les stories

--- packages/components/src/stories/index.ts
+++ packages/components/src/stories/index.ts
@@ -1,2 +1,11 @@
+// Core
 export * as BoxStory from '../core/Box/Box.stories';
+export * as ImageStory from '../core/Image/Image.stories';
+export * as MetabaseStory from '../core/Metabase/Metabase.stories';
 export * as TypographyStory from '../core/Typography/Typography.stories';
+export * as VersionStory from '../core/Version/Version.stories';
+
+// UI
+export * as ButtonStory from '../ui/Button/Button.stories';
+export * as DividerStory from '../ui/Divider/Divider.stories';
+export * as LucideIconStory from '../ui/LucideIcon/LucideIcon.stories';
