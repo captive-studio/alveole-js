cec0a4732c8a2ecd3ba8174bb17ed97544fbbef9
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-03-04 16:08:42 +0100

    🔖 release v0.15.5

--- packages/components/src/stories/index.ts
+++ packages/components/src/stories/index.ts
@@ -6,11 +6,11 @@ export * as TypographyStory from '../core/Typography/Typography.stories';
 export * as VersionStory from '../core/Version/Version.stories';
 
 // UI
+export * as AvatarStory from '../ui/Avatar/Avatar.stories';
 export * as ButtonStory from '../ui/Button/Button.stories';
 export * as DividerStory from '../ui/Divider/Divider.stories';
-export * as LucideIconStory from '../ui/LucideIcon/LucideIcon.stories';
-export * as ToolbarTopStory from '../ui/ToolbarTop/ToolbarTop.stories';
-export * as ToolbarBottomNavStory from '../ui/ToolbarBottomNav/ToolbarBottomNav.stories';
 export * as ListItemStory from '../ui/ListItem/ListItem.stories';
+export * as LucideIconStory from '../ui/LucideIcon/LucideIcon.stories';
 export * as ResourceListStory from '../ui/ResourceList/ResourceList.stories';
-export * as AvatarStory from '../ui/Avatar/Avatar.stories';
+export * as ToolbarBottomNavStory from '../ui/ToolbarBottomNav/ToolbarBottomNav.stories';
+export * as ToolbarTopStory from '../ui/ToolbarTop/ToolbarTop.stories';
