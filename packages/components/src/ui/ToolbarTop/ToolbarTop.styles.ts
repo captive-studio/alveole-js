0386f3c7d92de98dc91f10a9477dd6d50d9bc90f
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-03-09 23:08:39 +0100

    💄  Ajout du variant Large pour le ToolbarTop

--- packages/components/src/ui/ToolbarTop/ToolbarTop.styles.ts
+++ packages/components/src/ui/ToolbarTop/ToolbarTop.styles.ts
@@ -1,38 +1,78 @@
 import { makeStyles } from '@alveole/theme';
 
-export const useStyles = makeStyles(({ text, spacing, color }) => ({
-  container: {
-    display: 'flex',
-    width: '100%',
-  },
-  containerWithBackground: {
-    backgroundColor: color.background.default,
-  },
-  toolbarHaut: {
+export const useStyles = makeStyles(({ text, color, spacing }) => ({
+  toolbarContainer: {
+    backgroundColor: 'white',
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
     alignItems: 'center',
-    width: '100%',
-    minHeight: spacing('300'),
+    paddingLeft: spacing('025'),
+    paddingRight: spacing('025'),
+    paddingTop: spacing('050'),
+    paddingBottom: spacing('050'),
   },
-  informations: {
-    display: 'flex',
+  compactLargetoolbarContainer: {
+    paddingTop: spacing('3V'),
+    paddingBottom: spacing('1W'),
     paddingLeft: spacing('2W'),
     paddingRight: spacing('2W'),
-    paddingBottom: spacing('050'),
+  },
+  largeToolbarContainer: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  toolbarNavigation: {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: spacing('075'),
+  },
+  toolbarInformation: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: spacing('075'),
+    flex: 1,
+  },
+  toolbarInformationTitle: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flex: 1,
   },
   compactLargeInformations: {
     paddingBottom: 0,
-    minHeight: 60,
     justifyContent: 'center',
   },
-  titre: {
-    ...text.Titres['H1 - XL'],
+  toolbarInformationTitleText: {
+    ...text['Corps de texte'].SM.SemiBold,
     color: color.light.text['title-grey'],
   },
-  sousTitre: {
-    ...text['Corps de texte'].SM.Regular,
+  largeInformationTitleText: {
+    ...text.Titres['H1 - XL'],
+  },
+  toolbarInformationTitleSubText: {
+    ...text['Corps de texte'].XS.Regular,
     color: color.light.text['mention-grey'],
+  },
+  largeToolbarInformationTitleSubText: {
+    ...text['Corps de texte'].SM.Regular,
+  },
+  toolbarActions: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  toolbarInformationContainer: {
+    flex: 1,
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 }));
