e636d44f7e3ea4aaa103e6263fccd274c2bfc45b
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-03-13 12:29:09 +0100

    🐛 Exporte le composant Page

--- packages/components/src/core/index.ts
+++ packages/components/src/core/index.ts
@@ -1,6 +1,7 @@
 export * from './Box';
 export * from './Image';
 export * from './Metabase';
+export * from './Page';
 export * from './Section';
 export * from './Typography';
 export * from './Version';
