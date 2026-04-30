45b59e2e0a090b25c64d18f9ddb28890bf3cc576
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-04-30 10:42:47 +0200

    📝 Ajoute un exemple de Link Block pour le composant A

--- packages/components/src/core/A/A.stories.tsx
+++ packages/components/src/core/A/A.stories.tsx
@@ -1,4 +1,6 @@
+import { Avatar, Card } from '@alveole/components';
 import type { Story } from '../../type/Story';
+import { Box } from '../Box';
 import { Typography } from '../Typography';
 import { A } from './A';
 import { useStyles } from './A.styles';
@@ -7,14 +9,27 @@ export default {
   title: 'A',
   tags: ['core'],
   experimental: false,
-  description: 'Equivalent d’une balise <a>',
+  description: 'Equivalent d’une balise <a>. Envelopper le texte dans une balise <Typography>.',
   component: A,
   styleFn: useStyles,
 } satisfies Story;
 
 export const Default = () => (
   <A href={'/ui-kit/components/A'}>
-    <Typography>Children</Typography>
+    <Typography>Mon lien</Typography>
+  </A>
+);
+
+export const LinkBlock = () => (
+  <A href={'/ui-kit/components/A'}>
+    <Card>
+      <Card.Header
+        titre="Titre de la carte"
+        sousTitre="Sous-titre de la carte"
+        image={<Avatar size="md" fallbackText="M" />}
+      />
+      <Card.Section titre="Titre de la carte" titreIcone="Calendar" description="Description de la carte" />
+    </Card>
   </A>
 );
 
